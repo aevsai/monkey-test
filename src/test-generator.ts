603151f19d50c660956cd3d7627aecb9c4d21f38
aevsai <a.evsikov.ru@gmail.com>
2025-10-30 18:40:03 +0200

    Update test-generator.ts

--- src/test-generator.ts
+++ src/test-generator.ts
@@ -177,7 +177,7 @@ async function callLLMWithRetry(
           },
         ],
         temperature: 0.7,
-        max_tokens: 4000,
+        max_completion_tokens: 4000,
       });
 
       const content = response.choices[0]?.message?.content;
